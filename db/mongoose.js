7f0aa42518552d5942d3c025f15b7f8865bb841e
mvogt <moritz.vogt@vogges.de>
2018-09-19 21:45:52 +0200

    Initial commit: - Setup conig - Setup authentification - Setupt mongoose

---
+++ db/mongoose.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+// Mongoose uses now Promises
+mongoose.Promise = global.Promise;
+
+// Connect to Database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/BaseBackend');
+
+module.exports = { mongoose };
+
+
--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-require('./config/config');
+// --- CONFIG ---
+require('./../config/config');
+
+// +++ THIRD PARTY MODULES +++
+var express = require('express');
+
+// +++ LOCAL +++
+var { User } = require('./../models/user');
+
+var app = express();
+
+// Declare Port for deployment or local
+const port = process.env.PORT || 3000;
+
+// Start of for NodeJs
+app.listen( port, () => {
+    console.log(`Started up on port ${port}`);
+});
+
+module.exports = {
+    app
+};
