5fbc8b5d645cd4a6ea5d55f69977e05af0c7f1b6
mvogt <moritz.vogt@vogges.de>
2018-09-20 07:07:16 +0200

    Initial commit: - added item model - added post item route

---
+++ models/item.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+// ----------------- MODEL -----------------
+var ItemSchema = new mongoose.Schema({
+    headline: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 256,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true,
+    },
+    price: {
+        type: String        ,
+        required: true,
+        minLength: 1
+    },
+    createdAt: {
+        type: String,
+        default: null
+    },
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+});
+
+
+var Item = mongoose.model('Item', ItemSchema);
+
+module.exports = { Item }
--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ require('./../config/config');
 var express = require('express');
 const cors = require('cors');
 const _ = require('lodash');
+var moment = require('moment');
+var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
 
 // +++ LOCAL +++
 var { mongoose } = require('./../db/mongoose').mongoose;
 var { User } = require('./../models/user');
+var { Item } = require('./../models/item');
 var { authenticate } = require('./../middleware/authenticate');
-var bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+
+
 
 var app = express();
 
@@ -65,6 +70,29 @@ app.delete( '/users/me/token', authenticate, async ( req, res ) => {
         res.status( 200 ).send();
     } catch (e) {
         res.status( 400 ).send()
+    }
+});
+
+app.post('/item', authenticate, async ( req, res ) => {
+    try {
+        var date = new Date().getTime();
+        // Todo - Format in Local Date Format
+        var formattedTime = moment( date ).format('DD.MM.YYYY');
+
+        var item = new Item({
+            headline: req.body.headline,
+            description: req.body.description,
+            createdAt: formattedTime,
+            price: req.body.price,
+            _creator: req.user._id
+        });
+
+        item.save().then( ( item ) => {
+            res.send( item )
+        })
+    } catch (e) {
+        res.status( 400 ).send( "Something went wrong during saving the Item");
+
     }
 });
 
